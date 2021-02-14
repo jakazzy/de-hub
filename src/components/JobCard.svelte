2759690ac0d8a46c21a548e384df19b77843b67f
jakazzy <jakazzy@gmail.com>
2021-02-14 17:11:42 +0000

    add job list page

---
+++ src/components/JobCard.svelte
@@ -0,0 +1,128 @@
+<script>
+  export let job = {};
+</script>
+
+<div class="job-card">
+  <div class="job-card__logo">
+    <div class="logo">
+      <p>{job.company}</p>
+    </div>
+  </div>
+
+  <div class="job-card__Details">
+    <div class="job-card__Details-header">
+      <p class="company-name" style="margin-top: 20px;">{job.company}</p>
+      <p class="new-tag">New!</p>
+      <p class="featured-tag">Featured</p>
+    </div>
+
+    <div class="job-card__Details-name">
+      <p>{job.position}</p>
+    </div>
+
+    <div class="job-card__Details-footer">
+      <p class="posting-date">{job.postedAt}</p>
+      <p class="job-state">{job.contract}</p>
+      <p class="job-location">{job.location}</p>
+    </div>
+  </div>
+  <div class="job-card__tags">
+    <p class="tag">{job.role}</p>
+    <p class="tag">{job.level}</p>
+    {#each job.languages as lang}
+      <p class="tag">{lang}</p>
+    {/each}
+    {#each job.tools as tool}
+      <p class="tag">{tool}</p>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .job-card {
+    width: 60%;
+    height: 13rem;
+    margin: 3rem auto;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    display: flex;
+    border-left: 5px solid rgba(94, 164, 165, 255);
+    border-radius: 5px;
+  }
+
+  .job-card__logo {
+    width: 20%;
+    position: relative;
+  }
+
+  .job-card__Details {
+    width: 40%;
+  }
+
+  .job-card__tags {
+    width: 35%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .logo {
+    width: 7rem;
+    height: 7rem;
+    text-align: center;
+    border-radius: 50%;
+    background-color: #000;
+    color: #fff;
+    position: absolute;
+    top: 50%;
+    left: 20%;
+    transform: translateY(-50%);
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+  }
+
+  .job-card__Details-header {
+    display: flex;
+    justify-content: space-between;
+    width: 65%;
+  }
+
+  .new-tag {
+    background-color: #7db5b4;
+    width: 3rem;
+  }
+  .featured-tag {
+    background-color: #000;
+    width: 5rem;
+  }
+
+  .new-tag,
+  .featured-tag {
+    text-align: center;
+    color: white;
+    padding: 0.3rem;
+    border-radius: 20px;
+  }
+
+  .job-card__Details-name {
+    font-weight: 500;
+    font-size: 1.3rem;
+  }
+
+  .job-card__Details-footer {
+    display: flex;
+    justify-content: space-between;
+    width: 65%;
+    color: #8e9696;
+  }
+
+  .tag {
+    background-color: rgba(219, 232, 232, 255);
+    color: #65b9ba;
+    height: 1rem;
+    padding: 0.3rem;
+    border-radius: 8px;
+    text-align: center;
+  }
+</style>
---
+++ src/components/JobCardList.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from "svelte";
+  import JobCard from "./JobCard.svelte";
+  import data from "../static-job-listings-master/data.js";
+
+  let jobData = [];
+
+  onMount(() => {
+    jobData = data();
+  });
+</script>
+
+{#each jobData as job (job.id)}
+  <JobCard {job} />
+{/each}
